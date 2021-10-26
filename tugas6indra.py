8093446ba4b41c9c225776e9bee2ea05a96988a0
IndraWahyu312110399 <indrawahyu1456@gmail.com>
2021-10-26 12:51:08 +0700

    add tugas6indra.py

---
+++ tugas6indra.py
@@ -0,0 +1,15 @@
+#Nama : Indra
+#Kelas: TI.21.A.1
+#Tugas: Bahasa Pemrograman
+#Tugas Menghitung Luas Dan Keliling Lingkaran
+#load library
+
+import math
+
+r = float(input("Masukan Jari-jari : "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("Luas Lingkaran \t\t= ",luas)
+print ("Keliling Lingkaran\t= ",keliling)
